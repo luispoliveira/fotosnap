f30c04b93bc3bd1223e9965e3ddfe314f4046444
Luis Oliveira <luispedroliveira96@gmail.com>
2026-03-06 15:39:01 +0000

    feat: implement CommentsRouter with create, find, and delete comment functionalities

---
+++ apps/backend/src/comments/comments.router.ts
@@ -0,0 +1,67 @@
+import {
+  commentSchema,
+  CreateCommentInput,
+  createCommentSchema,
+  DeleteCommentInput,
+  deleteCommentSchema,
+  GetCommentsInput,
+  getCommentsSchema,
+} from '@repo/trpc/schemas';
+import {
+  Ctx,
+  Input,
+  Mutation,
+  Query,
+  Router,
+  UseMiddlewares,
+} from 'nestjs-trpc-v2';
+import { AppContext } from 'src/app-context.interface';
+import { AuthTrpcMiddleware } from 'src/auth/auth-trpc.middleware';
+import z from 'zod';
+import { CommentsService } from './comments.service';
+
+@Router()
+@UseMiddlewares(AuthTrpcMiddleware)
+export class CommentsRouter {
+  constructor(private readonly commentsService: CommentsService) {}
+
+  @Mutation({
+    input: createCommentSchema,
+  })
+  async createComment(
+    @Input() createCommentDto: CreateCommentInput,
+    @Ctx() context: AppContext,
+  ) {
+    return this.commentsService.createComment(
+      createCommentDto,
+      context.user.id,
+    );
+  }
+
+  @Query({
+    input: getCommentsSchema,
+    output: z.array(commentSchema),
+  })
+  async findByPostId(
+    @Input() getCommentsInput: GetCommentsInput,
+    @Ctx() context: AppContext,
+  ) {
+    return this.commentsService.findByPostId(
+      getCommentsInput.postId,
+      context.user.id,
+    );
+  }
+
+  @Mutation({
+    input: deleteCommentSchema,
+  })
+  async deleteComment(
+    @Input() deleteCommentInput: DeleteCommentInput,
+    @Ctx() context: AppContext,
+  ) {
+    return this.commentsService.deleteComment(
+      deleteCommentInput.commentId,
+      context.user.id,
+    );
+  }
+}
